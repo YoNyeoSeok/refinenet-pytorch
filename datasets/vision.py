6619a23cb127745762630a854f3b08900ba6626d
YoNyeoSeok <yys8646@postech.ac.kr>
2020-05-21 12:06:58 +0000

    Update ConcatVisionDataset, StackVisionDataset

--- datasets/vision.py
+++ datasets/vision.py
@@ -5,6 +5,7 @@ import zipfile
 
 from torchvision.datasets.utils import extract_archive, verify_str_arg, iterable_to_str
 from torch.utils.data import Dataset
+from utils.data import ConcatDataset, StackDataset
 from PIL import Image
 
 
@@ -59,13 +60,13 @@ class VisionDataset(Dataset):
         return len(self.images)
 
     def __repr__(self):
-        head = "Dataset " + self.__class__.__name__
+        head = self.__class__.__name__
         body = ["Number of datapoints: {}".format(self.__len__())]
         if self.root is not None:
             body.append("Root location: {}".format(self.root))
         body += self.extra_repr().splitlines()
-        if hasattr(self, "transforms") and self.transforms is not None:
-            body += [repr(self.transforms)]
+        if self.transform is not None:
+            body += self._format_transform_repr(self.transform, "Transform: ")
         lines = [head] + [" " * self._repr_indent + line for line in body]
         return '\n'.join(lines)
 
@@ -76,3 +77,75 @@ class VisionDataset(Dataset):
 
     def extra_repr(self):
         return ""
+
+    
+class ConcatVisionDataset(ConcatDataset):
+    _repr_indent = VisionDataset._repr_indent
+    __repr__ = VisionDataset.__repr__
+    _format_transform_repr = VisionDataset._format_transform_repr
+    extra_repr = VisionDataset.extra_repr
+
+    def __init__(self, *args, **kwds):
+        super(ConcatVisionDataset, self).__init__(*args, **kwds)
+        self.root = self.datasets[0].root
+        self.substring = self.datasets[0].substring
+        self.convert = self.datasets[0].convert
+        self.transform = self.datasets[0].transform
+        # self.extra_repr = self.datasets[0].extra_repr
+
+        for i, d in enumerate(self.datasets):
+            assert self.root == d.root, "datasets should have same root"
+            assert self.substring==d.substring, "datasets should have same substring"
+            assert self.convert==d.convert, "datasets should have same convert"
+            assert self.transform==d.transform, "datasets should have same transform"
+            # assert self.extra_repr()==d.extra_repr(), "datasets should have same extra_repr"
+        
+    @property
+    def images(self):
+        images = self.datasets[0].images
+        for dataset in self.datasets[1:]:
+            images += dataset.images
+        return images
+    
+        
+class StackVisionDataset(StackDataset):
+    _repr_indent = VisionDataset._repr_indent
+    _format_transform_repr = VisionDataset._format_transform_repr
+    extra_repr = VisionDataset.extra_repr
+
+    def __init__(self, *args, **kwds):
+        super(StackVisionDataset, self).__init__(*args, **kwds)
+        self.root = self.datasets[0].root
+        # self.extra_repr = self.datasets[0].extra_repr
+
+        for i, d in enumerate(self.datasets):
+            assert self.root == d.root, "datasets should have same root"
+            # assert self.extra_repr()==d.extra_repr(), "datasets should have same extra_repr"
+    
+    @property
+    def images(self):
+        return list(zip(*[dataset.images for dataset in self.datasets]))
+    
+    def __repr__(self):
+        head = self.__class__.__name__
+        body = ["Number of datapoints: {}".format(self.__len__())]
+        if self.root is not None:
+            body.append("Root location: {}".format(self.root))
+        body += self.extra_repr().splitlines()
+        def transform_body(d, head):
+            if hasattr(d, 'transform'):
+            # if isinstance(d, (VisionDataset, ConcatVisionDataset)):
+                return self._format_transform_repr(d.transform, head)
+            elif hasattr(d, 'datasets'):
+            # elif isinstance(d, StackVisionDataset):
+                return [head] + sum([transform_body(d_, "{}({}): ".format(" " * len(head), i_))
+                                     for i_, d_ in enumerate(d.datasets)],
+                                    [])
+            elif hasattr(d, 'dataset'):
+            # if isinstance(d, SubsetVision):
+                return transform_body(d.dataset, head)
+            else:
+                return []
+        body += transform_body(self, "Transform:")
+        lines = [head] + [" " * self._repr_indent + line for line in body]
+        return '\n'.join(lines)
